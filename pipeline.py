3c6057a2a8732f077d3072b511eb7aee19382a13
Nimisha Mittal <nimishamittal@Nimishas-MacBook-Pro.local>
2022-11-22 15:11:43 -0800

    Complete working pipeline

--- pipeline.py
+++ pipeline.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import re
 import nltk
+from sklearn.metrics.pairwise import linear_kernel
 
 from pipeline_utils import *
 
@@ -61,7 +62,7 @@ class Pipeline:
         self.tfidf_vec = tfidf(self.df, single_doc=False)
         self.bert_vec = bert(self.df, single_doc=False)
 
-        print("CHECKING.....")
+        print("CHECKING......")
         print("TF-IDF",self.tfidf_vec.shape, self.tfidf_vec[0].shape)
         print("Doc2Vec",self.d2v_vec.shape, self.d2v_vec[0].shape)
         print("BERT",self.bert_vec.shape, self.bert_vec[0].shape)
@@ -78,25 +79,38 @@ class Pipeline:
 
     def get_similar_documents(self):
 
-        # get cosine similarity with tfidf
-        print(self.doc_tfidf.shape, self.tfidf_vec.shape)
-        print(self.doc_d2v.shape, self.d2v_vec.shape)
-        print(self.doc_bert.shape, self.bert_vec.shape)
+        self.doc_tfidf = self.doc_tfidf.reshape(self.doc_tfidf.shape[0], 1).T
+        self.doc_d2v = self.doc_d2v.reshape(self.doc_d2v.shape[0], 1).T
+        self.doc_bert = self.doc_bert.reshape(self.doc_bert.shape[0], 1).T
 
-
-        print('#'*50)
+ 
+        print("TF-IDF SIMILARITY")
         cosine_similarities_tfidf = cosine_similarity(self.doc_tfidf, self.tfidf_vec).flatten()
+        related_docs_tfidf = cosine_similarities_tfidf.argsort()
+        # [-5:][::-1]
+        print(related_docs_tfidf)
 
-        #cosine_similarities = linear_kernel(X_data[case_id:case_id+1], X_data).flatten()
-        related_docs_indices = cosine_similarities_tfidf.argsort()[:-5:-1]
-        print(related_docs_indices)
+
+        print("Doc2Vec SIMILARITY")
+        cosine_similarities_d2v = cosine_similarity(self.doc_d2v, self.d2v_vec).flatten()
+        related_docs_d2v = cosine_similarities_d2v.argsort()
+        # [-5:][::-1]
+        print(related_docs_d2v)
 
 
-pipeline_obj = Pipeline('check.txt', 'data/all_year.pkl.zst', run_type=True)
-pipeline_obj.get_similar_documents()
+        print("BERT SIMILARITY")
+        cosine_similarities_bert = cosine_similarity(self.doc_bert, self.bert_vec)[0]
+        related_docs_bert = cosine_similarities_bert.argsort()
+        # [-5:][::-1]
+        print(related_docs_bert)
+
+
+if __name__ == '__main__':
+    pipeline_obj = Pipeline('check.txt', 'data/all_year.pkl.zst', run_type=False)
+    pipeline_obj.get_similar_documents()
+            
+
         
-
-    
 
     
 
